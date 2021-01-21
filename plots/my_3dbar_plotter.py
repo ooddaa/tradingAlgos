fbcc96ddc96fc8ff68b49a83cfa9140327634a66
Dima <dv@kappacoding.co.uk>
2021-01-21 22:32:28 +0000

    3dbar plot of 3days pltr open interest 6mo all strikes looks super-ugly

--- plots/my_3dbar_plotter.py
+++ plots/my_3dbar_plotter.py
@@ -1,13 +1,5 @@
-def df_combiner(dfs):
-    for i in range(len(dfs)):
-        if i < len(dfs)-1:
-            if i == 0:
-                x = dfs[i]
-            x = x.combine_first(dfs[i+1])
-    return x
-
 def df_to_3Dbars(misdf):
-    # MIS DataFrame is 
+    # MIS DataFrame is
     # rows == days of observations
     # cols = expirations
     # values = MIS
@@ -17,9 +9,9 @@ def df_to_3Dbars(misdf):
         for col in misdf.columns:
             bar = [col, idx, misdf.loc[idx, col]]
             arr.append(bar)
-    
-    x = [] 
-    y = [] 
+
+    x = []
+    y = []
     z = []
 
     for bar in arr:
@@ -27,26 +19,28 @@ def df_to_3Dbars(misdf):
         x.append(col)
         y.append(idx)
         z.append(strike)
-        
+
     return x, y, z
 
+
 def plotter(x, y, z, width=1, depth=1, ax=False, figsize=False, labels=['x', 'y', 'z']):
-    
+
     # defaults
-    figsize = (10,10) if figsize is False else figsize
-    
+    figsize = (10, 10) if figsize is False else figsize
+
     if ax is False:
-        fig = plt.figure(figsize=(10,10))
+        fig = plt.figure(figsize=(10, 10))
         ax = fig.add_subplot(111, projection='3d')
-        
+
     bottom = np.zeros_like(z)
     hight = z
-    
+
     ax.set_xlabel(labels[0])
     ax.set_ylabel(labels[1])
     ax.set_zlabel(labels[2])
     out = ax.bar3d(x, y, bottom, width, depth, hight)
     return out
+
 
 def my_3dbar_plotter(misdf, **kwargs):
     x, y, z = df_to_3Dbars(misdf)
