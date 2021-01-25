00bc65d0122848f91b7fe902812b38754f159e7c
Dima <dv@kappacoding.co.uk>
2021-01-25 15:13:28 +0000

    heatmap ok, need multiple heatmaps plotter

--- plots/my_3dbar_plotter.py
+++ plots/my_3dbar_plotter.py
@@ -7,7 +7,7 @@ def df_to_3Dbars(misdf):
 
     for idx in misdf.index:
         for col in misdf.columns:
-            bar = [col, idx, misdf.loc[idx, col]]
+            bar = [idx, col, misdf.loc[idx, col]]
             arr.append(bar)
 
     x = []
@@ -15,33 +15,65 @@ def df_to_3Dbars(misdf):
     z = []
 
     for bar in arr:
-        col, idx, strike = bar
-        x.append(col)
-        y.append(idx)
+        idx, col, strike = bar
+        x.append(idx)
+        y.append(col)
         z.append(strike)
 
     return x, y, z
 
 
-def plotter(x, y, z, width=1, depth=1, ax=False, figsize=False, labels=['x', 'y', 'z']):
+def plotter(x, y, z,
+            width=1,
+            depth=1,
+            ax=False,
+            figsize=False,
+            labels=['x', 'y', 'z'],
+            xticks=False,
+            xticklabels=False
+            ):
 
     # defaults
     figsize = (10, 10) if figsize is False else figsize
 
     if ax is False:
-        fig = plt.figure(figsize=(10, 10))
+        fig = plt.figure(figsize=figsize)
         ax = fig.add_subplot(111, projection='3d')
 
     bottom = np.zeros_like(z)
     hight = z
 
-    ax.set_xlabel(labels[0])
+    # ax.set_xlabel(labels[0])
     ax.set_ylabel(labels[1])
     ax.set_zlabel(labels[2])
+
+    # set xticks and xticks labels
+    # https://matplotlib.org/3.1.3/api/_as_gen/matplotlib.axes.Axes.set_xticks.html
+    # https://stackoverflow.com/questions/14852821/aligning-rotated-xticklabels-with-their-respective-xticks
+    if xticks is not False:
+        print('len xticks:', len(xticks))
+        ax.set_xticks(xticks,
+                      minor=False
+                      )
+    else:
+        ax.set_xticks(range(0, len(x)))  # or default
+
+    if xticklabels is not False:
+        # https://matplotlib.org/3.3.2/api/_as_gen/matplotlib.axes.Axes.set_xticklabels.html
+        # print('xticklabels:', xticklabels)
+        ax.set_xticklabels(xticklabels,
+                           rotation=45,
+                           ha='right'
+                           )
+
     out = ax.bar3d(x, y, bottom, width, depth, hight)
     return out
 
 
 def my_3dbar_plotter(misdf, **kwargs):
-    x, y, z = df_to_3Dbars(misdf)
-    return plotter(x, y, z, **kwargs)
+    x, y, z = df_to_3Dbars(misdf.loc[:, misdf.columns != 'index'])
+    return plotter(x, y, z,
+                   xticks=range(0, len(misdf['index'])),
+                   xticklabels=misdf['index'],
+                   **kwargs
+                   )
---
+++ plots/my_heatmap_plotter.py
@@ -0,0 +1,29 @@
+
+
+def my_heatmap_plotter(x, y, z, zlabel='Open Interest',
+                       ax=False,
+                       figsize=False,
+                       title='Most Interesting Contract'
+                       ):
+    # defaults
+    figsize = (10, 10) if figsize is False else figsize
+
+    if ax is False:
+        fig, ax = plt.subplots(figsize=figsize)
+
+    im = ax.imshow(z)
+
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, ax=ax)
+    cbar.ax.set_ylabel(zlabel, rotation=-90, va="bottom")
+
+    # We want to show all ticks...
+    ax.set_xticks(np.arange(len(x)))
+    ax.set_yticks(np.arange(len(y)))
+    # ... and label them with the respective list entries
+    ax.set_xticklabels(x, rotation=45, ha="right",
+                       rotation_mode="anchor")
+    ax.set_yticklabels(y)
+
+    ax.set_title(title)
+    fig.tight_layout()
